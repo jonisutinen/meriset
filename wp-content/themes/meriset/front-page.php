0bec804439e3c842387c4b26ad781fb7c3cf525f
Joni Sutinen <sutinen.joni@gmail.com>
2020-05-24 23:18:49 +0300

    front page cards

--- wp-content/themes/meriset/front-page.php
+++ wp-content/themes/meriset/front-page.php
@@ -4,118 +4,110 @@
     $carousel = get_field('karuselli');
 ?>
 <div id="frontPageCarousel" class="carousel slide front--page--carousel" data-ride="carousel">
-      <div class="carousel-inner">
-        
-        
+    <div class="carousel-inner">
+
+
         <div class="carousel-item active">
-          <img src="<?php echo esc_url($carousel['karuselli_kuva_1']['url'])?>" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-          </div>
+            <img src="<?php echo esc_url($carousel['karuselli_kuva_1']['url'])?>" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+            </div>
         </div>
         <?php if($carousel['karuselli_kuva_2']): ?>
         <div class="carousel-item">
-          <img src="<?php echo esc_url($carousel['karuselli_kuva_2']['url'])?>" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-          </div>
+            <img src="<?php echo esc_url($carousel['karuselli_kuva_2']['url'])?>" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+            </div>
         </div>
         <?php endif; ?>
         <?php if($carousel['karuselli_kuva_3']): ?>
         <div class="carousel-item">
-          <img src="<?php echo esc_url($carousel['karuselli_kuva_2']['url'])?>" class="d-block w-100" alt="...">
-          <div class="carousel-caption d-none d-md-block">
-          </div>
+            <img src="<?php echo esc_url($carousel['karuselli_kuva_2']['url'])?>" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+            </div>
         </div>
         <?php endif; ?>
-      </div>
     </div>
-    <div class="content">
+</div>
+<div class="content">
     <div class="container">
-    <!-- <div class="card-group"> -->
-      <div class="row row-cols-1 row-cols-lg-3">
-          <div class="col mb-4">
+        <!-- <div class="card-group"> -->
+        <div class="row row-cols-1 row-cols-lg-3">
+            <?php 
+                $card1 = get_field('kortti_1');
+                $card2 = get_field('kortti_2');
+                $card3 = get_field('kortti_3');
+                if ($card1):
+            ?>
 
-      <div class="card">
-        <img src="<?php bloginfo('template_directory'); ?>/img/haapasaarilinnut.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Syysartika</h3>
-          <p class="card-text">COVID-19-pandemiasta johtuen risteilykautemme siirtyy ja liikenne on keskeytetty 31.5.2020 asti.
-          <br><br>
-          Seuraamme tiiviisti viranomaisten tiedotteita. Kun tilanne taas sallii irtautua laiturista, aloitamme välittömästi
-          liikennöinnin.
-          <br><br>
-          Meihin voi olla kuitenkin yhteydessä säköpostitse tai puhelimitse. Suunnitellaan yhdessä loppukesän risteilypaketteja
-          tai varaa ryhmällesi merielämys jo vuodelle 2021.
-          <br><br>
-          Kaikesta huolimatta nautitaan keväästä ja valosta. Pysykää terveinä!</p>
-          <div class="button-container">
-          <a href="#" class="btn btn-primary">Lue lisää</a>
-          </div>
-        </div>
-        <!-- <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div> -->
-      </div>
-    </div>
-      <div class="col mb-4">
+            <div class="col mb-4">
 
-      <div class="card">
-        <img src="<?php bloginfo('template_directory'); ?>/img/tammionranta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Tammion kalastajakylä</h3>
-          <p class="card-text">Tammion reittiliikenne on keskeytetty COVID-19- pandemian takia 31.5.2020 asti.
-          <br><br>
-          Seuraamme tiiviisti viranomaisten tiedotteita ja toivomme pääsevämme aloittamaan reittiliikenteen Tammioon niin pian
-          kuin mahdollista.
-          <br><br>
-          Nyt on aika pitää huolta itsestämme ja läheisistämme!
-          <br><br>
-          Kyllä tämäkin myrsky selätetään ja irrotetaan köydet entistä vahvempina.</p>
-          <div class="button-container">
-            <a href="#" class="btn btn-primary">Lue lisää</a>
-          </div>
-        
-        </div>
-        <!-- <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div> -->
-      </div>
-      </div>
-      <div class="col mb-4">
+                <div class="card">
 
-      <div class="card">
-        <img src="<?php bloginfo('template_directory'); ?>/img/vikla1joki.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h3 class="card-title">Yleisön suosikki: jokiristeilyt</h3>
-          <p class="card-text">Risteilyn lähtö- ja paluupaikat ovat kahden ainutlaatuisen puiston kyljessä.
-          <br><br>
-          Tällä risteilyllä nautitaan sekä merimaisemista että joen virtaavasta kauneudesta.</p>
-          <div class="button-container">
-        <a href="#" class="btn btn-primary">Lue lisää</a>
-        </div>
-        </div>
+                    <img src="<?php echo esc_url($card1['kortti_kuva']['url']);?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title"><?php echo $card1['kortti_otsikko'];?></h3>
+                        <p class="card-text"><?php echo $card1['kortti_teksti'];?></p>
+                        <?php if($card1['kortti_nappi_linkki']): ;?>
+                        <div class="button-container">
+                            <a href="<?php echo $card1['kortti_nappi_linkki'] ;?>"
+                                class="btn btn-primary"><?php echo $card1['kortti_nappi_teksti'];?></a>
+                        </div>
+                        <?php endif;?>
+                    </div>
 
-        <!-- <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div> -->
-      </div>
-      </div>
-      <div class="col mb-4">
-      
-        <div class="card">
-          <img src="<?php bloginfo('template_directory'); ?>/img/vikla1joki.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h3 class="card-title">Yleisön suosikki: jokiristeilyt</h3>
-            <p class="card-text">Risteilyn lähtö- ja paluupaikat ovat kahden ainutlaatuisen puiston kyljessä.
-              <br><br>
-              Tällä risteilyllä nautitaan sekä merimaisemista että joen virtaavasta kauneudesta.</p>
-            <div class="button-container">
-              <a href="#" class="btn btn-primary">Lue lisää</a>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-    </div>
+            <?php 
+                endif; 
+                if ($card2):
+            ?>
 
-    <?php get_footer(); ?>
+            <div class="col mb-4">
+
+                <div class="card">
+
+                    <img src="<?php echo esc_url($card2['kortti_kuva']['url']);?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title"><?php echo $card2['kortti_otsikko'];?></h3>
+                        <p class="card-text"><?php echo $card2['kortti_teksti'];?></p>
+                        <?php if($card2['kortti_nappi_linkki']): ;?>
+                        <div class="button-container">
+                            <a href="<?php echo $card2['kortti_nappi_linkki'] ;?>"
+                                class="btn btn-primary"><?php echo $card2['kortti_nappi_teksti'];?></a>
+                        </div>
+                        <?php endif;?>
+                    </div>
+
+                </div>
+            </div>
+            <?php 
+                endif; 
+                if ($card3):
+            ?>
+            <div class="col mb-4">
+
+                <div class="card">
+
+                    <img src="<?php echo esc_url($card3['kortti_kuva']['url']);?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title"><?php echo $card3['kortti_otsikko'];?></h3>
+                        <p class="card-text"><?php echo $card3['kortti_teksti'];?></p>
+                        <?php if($card3['kortti_nappi_linkki']): ;?>
+                        <div class="button-container">
+                            <a href="<?php echo $card3['kortti_nappi_linkki'] ;?>"
+                                class="btn btn-primary"><?php echo $card3['kortti_nappi_teksti'];?></a>
+                        </div>
+                        <?php endif;?>
+                    </div>
+
+                </div>
+            </div>
+            <?php 
+                endif; 
+            ?>
+            
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
